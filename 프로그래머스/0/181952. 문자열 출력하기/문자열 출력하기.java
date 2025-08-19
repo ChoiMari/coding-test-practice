f140c4fb1b246702f46a36efe0890474e8cb300e
Mari <chl98639863@gmail.com>
2025-08-19 11:11:18 +0900

    [level 0] Title: 문자열 출력하기, Time: 218.06 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        // 문자열 출력
+        System.out.print(str);
+        
+        sc.close(); // 리소스 닫기
+    }
+}
