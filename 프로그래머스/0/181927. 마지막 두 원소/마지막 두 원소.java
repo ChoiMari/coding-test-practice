2e3870cbabd4a11c1ad5aaa74e8f125e6ad3a4d4
Mari <chl98639863@gmail.com>
2026-01-20 06:11:12 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,22 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] result = new int[num_list.length + 1];
-        int last = num_list.length - 1;  // 마지막 원소 인덱스
-        int before = num_list.length - 2; // 그 전 원소 인덱스
-        
-        //기존 값 복사
-        for(int i = 0; i < num_list.length ;i++){
-            result[i] = num_list[i];
-        }
-        
-        // 추가
-        if(num_list[last] > num_list[before]){
-            result[result.length - 1] = num_list[last] - num_list[before];
-        }else{
-            result[result.length - 1] = num_list[last] * 2;
-        }
-        
-        return result;
+        int[] answer = {};
+        return answer;
     }
 }
