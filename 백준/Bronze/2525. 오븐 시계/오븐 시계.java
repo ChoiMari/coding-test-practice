e3620cf65b8b9ee4a13cecc39dba7772de2bd2be
Mari <chl98639863@gmail.com>
2026-02-02 20:54:32 +0900

    [Bronze III] Title: 오븐 시계, Time: 152 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -3,19 +3,20 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        String[] startTimeStr = sc.nextLine().split("\\s+"); //시작 시간 분
-        int cookTimeMinutes = sc.nextInt(); // 요리에 필요한 소요시간
-        
-        int startHour = Integer.parseInt(startTimeStr[0]); // 시(정수변환)
-        int startMinutes = Integer.parseInt(startTimeStr[1]);// 분(정수변환)
-        
-        // 시작시간을 분으로 환산
+        int startHour = sc.nextInt(); //시작 시
+        int startMinutes = sc.nextInt(); // 시작 분
+        int needMinutes = sc.nextInt(); // 요리에 필요한 시간(분단위)
+       
+        // 요리 시작 시간을 전체 분으로 환산
         int startTimeMinutes = (startHour * 60) + startMinutes;
         
         //끝나는 시간 계산
-        int endCookTotalMinutes = startTimeMinutes + cookTimeMinutes;
-        if(endCookTotalMinutes >= (24 * 60)){
-            endCookTotalMinutes -= (24 * 60); 
+        int endCookTotalMinutes = startTimeMinutes + needMinutes;
+        if(endCookTotalMinutes >= 1440){
+            // 하루는 24 * 60 = 1440분
+            endCookTotalMinutes %= 1440;
+            // 하루가 몇 번 넘어가든 오늘 시각만 남길 수 있음
+            // 나머지가 0~1439로 시계처럼 순환
         }
         int endHour = endCookTotalMinutes / 60;
         int endMinutes = endCookTotalMinutes % 60;
