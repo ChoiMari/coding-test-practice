25ac1f1e283d8bf1fa4371d83b9850494d9527f2
Mari <chl98639863@gmail.com>
2026-02-26 15:11:28 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
-        double result = (double) num1 / num2 * 1000;
-        return (int) result;
+         // num1을 double로 변환하여 실수 나눗셈 수행
+        // 이후 1000을 곱한 뒤 int로 캐스팅하여 소수점 버림
+        return (int) ((double) num1 / num2 * 1000);
     }
 }
