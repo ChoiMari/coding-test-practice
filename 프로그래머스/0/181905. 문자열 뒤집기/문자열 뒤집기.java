30d121b78eda06367e1a743762db8e666f169aff
Mari <chl98639863@gmail.com>
2026-04-14 04:39:11 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.08 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,12 +1,6 @@
-import java.util.*;
 class Solution {
     public String solution(String my_string, int s, int e) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(my_string.substring(0, s));
-        String str = new StringBuilder(my_string.substring(s, e + 1)).reverse().toString();
-        //reverse()메서드 사용 위해서 StringBuilder객체로 감쌈
-        sb.append(str);
-        sb.append(my_string.substring(e + 1));
-        return sb.toString();
+        String answer = "";
+        return answer;
     }
 }
